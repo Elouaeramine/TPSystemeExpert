5b82b9160f114f0866185e39027e203f15a1aea3
Amine <elouaeramine266@hotmail.com>
2021-10-17 19:04:19 +0100

    split proposition

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 # This is a sample Python script.
-
+import re
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 def read_file(filename):
     file = open(filename,"r")
-    print(file.readlines())
     propositions = file.readlines()
     file.close()
     return propositions
 
-
+def split_propositions(propositions):
+    base_regles = []
+    for proposition in propositions:
+        print(f"{proposition} 22")
+        res = re.findall(r'\w+', proposition)
+        myDict = {"premisse": res[1], "conclusion": res[3]}
+        base_regles.append(myDict)
+    return base_regles
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    read_file("file.txt")
+    p1 = read_file("file.txt")
+    print(split_propositions(p1))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
