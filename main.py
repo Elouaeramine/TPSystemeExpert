9cc40da1258ac23f27e242df7296ec232ef132c1
Amine <elouaeramine266@hotmail.com>
2021-10-20 16:09:18 +0100

    detecter regles declenchables

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
-# This is a sample Python script.
+
 import re
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-def read_file(filename):
-    file = open(filename,"r")
+# This function will read a file and returns an array of propositions.
+def read_propositions(filename):
+    file = open(filename, "r")
     propositions = file.readlines()
     file.close()
     return propositions
 
+
+# This function will read a file and returns an array of faits.
+def read_base_faits(filename):
+    file = open(filename, 'r')
+    faits = file.readline()
+    res =faits.split(',')
+    file.close()
+    return res
+
+
+# This Function will split an array of propositions to an object of premisses & conclusions
 def split_propositions(propositions):
     base_regles = []
     for proposition in propositions:
@@ -16,8 +26,24 @@ def split_propositions(propositions):
         myDict = {"premisse": res[1], "conclusion": res[3]}
         base_regles.append(myDict)
     return base_regles
+
+# This Function will take into params base_faits & base_connaissance and will return any rule ( déclenchable )
+def regles_declenchables(bf , bc ):
+    declenchables = []
+    a = [d['premisse'] for d in bc]
+    for base in bf:
+        if base in a:
+            print (f'mawjouda in a${base}')
+            declenchables.append(base)
+    return declenchables
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    p1 = read_file("file.txt")
+    #Read base connaissance
+    p1 = read_propositions("file.txt")
     print(split_propositions(p1))
+    #Read base faits
+    f = read_base_faits("base_fait.txt")
+    print(f)
+    # Detecter les regles declenchables
+    print(regles_declenchables(f, split_propositions(p1)))
 
